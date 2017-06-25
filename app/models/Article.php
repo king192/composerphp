284e7fd19649c1366146796540a76ae3f723fb41
king <king@iyouths.org>
2017-06-25 13:25:38 +0800

    t

--- app/models/Article.php
+++ app/models/Article.php
@@ -1,30 +1,50 @@
 <?php
+
 /**
+
 * Article Model
+
 */
-class Article
+
+class Article extends Illuminate\Database\Eloquent\Model
+
 {
-  public static function first()
-  {
-    $connection = mysqli_connect("localhost","root","root");
-    if (!$connection) {
-      die('Could not connect: ' . mysqli_error($connection));
-    }
 
-    mysqli_set_charset($connection, "UTF8");
+  public $timestamps = false;
 
-    mysqli_select_db($connection, "mffc");
-
-    $result = mysqli_query($connection,"SELECT * FROM articles");
-
-    $res = '';
-    if ($row = mysqli_fetch_assoc($result)) {
-      // echo '<h1>'.$row["title"].'</h1>';
-      // echo '<p>'.$row["content"].'</p>';
-      $res = $row;
-    }
-
-    mysqli_close($connection);
-    return $res;
-  }
 }
+
+
+?>
+
+
+
+
+
+
+
+<?php
+// class Article
+// {
+//   public static function first()
+//   {
+//     $connection = mysqli_connect("localhost","root","root");
+//     if (!$connection) {
+//       die('Could not connect: ' . mysqli_error($connection));
+//     }
+
+//     mysqli_set_charset($connection, "UTF8");
+
+//     mysqli_select_db($connection, "mffc");
+
+//     $result = mysqli_query($connection,"SELECT * FROM articles");
+
+//     $res = '';
+//     if ($row = mysqli_fetch_assoc($result)) {
+//       $res = $row;
+//     }
+
+//     mysqli_close($connection);
+//     return $res;
+//   }
+// }
